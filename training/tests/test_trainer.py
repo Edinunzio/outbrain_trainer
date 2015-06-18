09053f4ace680af3155132fec9987df81d7ea923
Edinunzio <e.dinunzio@gmail.com>
2015-06-17 20:34:38 -0400

    updating unit tests

--- training/tests/test_trainer.py
+++ training/tests/test_trainer.py
@@ -7,19 +7,35 @@ from training.trainer import Trainer
 class TestOutbrainTrainer(unittest.TestCase):
     def setUp(self):
         self.q_file = '../data/query_groups.txt'
-        self.i_file = '../data/input_groups.txt'
+        self.i_file = '../data/input_groups_2.txt'
+        self.tr = Trainer()
 
-
-    def test_read_file_returns_list(self):
-        tr = Trainer()
-        data = tr.read_file(self.q_file)
+    def test_read_file(self):
+        data = self.tr.read_file(self.q_file)
         self.assertEqual(type(data), list)
+        for line in data:
+            self.assertEqual(type(line), list)
+            for word in line:
+                self.assertEqual(type(word), str)
 
     def test_output(self):
-        tr = Trainer()
         data = {"blue": 1, "red": 2, "yellow": 1}
-        result = tr.output(data)
+        result = self.tr.output(data)
         self.assertEqual(type(result), str)
+
+    def test_get_query(self):
+        results = self.tr.get_query(self.q_file)
+        for r in results:
+            self.assertEqual(type(r), set)
+
+    def test_get_inputs(self):
+        results = self.tr.get_query(self.i_file)
+        for r in results:
+            self.assertEqual(type(r), set)
+
+    def test_match_query_input(self):
+        results = self.tr.match_query_input(self.i_file, self.q_file)
+        self.assertEqual(type(results), str)
 
 if __name__ == '__main__':
     unittest.main()
--- training/trainer.py
+++ training/trainer.py
@@ -1,13 +1,15 @@
 from json import dumps
 from collections import Counter
 
+
 class Trainer():
     def __init__(self):
         self.q_file = 'data/query_groups.txt'
         self.i_file = 'data/input_groups_2.txt'
         self.final_output = []
 
-    def read_file(self, filename):
+    @staticmethod
+    def read_file(filename):
         groups = []
         f = open(filename, 'r')
         for line in f:
@@ -17,36 +19,36 @@ class Trainer():
         f.close()
         return groups
 
-    def get_query(self):
-        queries = self.read_file(self.q_file)
+    def get_query(self, filename):
+        queries = self.read_file(filename)
         for q_group in queries:
             yield set(q_group)
 
-    def get_inputs(self):
-        inputs = self.read_file(self.i_file)
+    def get_inputs(self, filename):
+        inputs = self.read_file(filename)
         for i_group in inputs:
             yield set(i_group)
 
-    def match_query_input(self):
-        inputs = self.get_inputs()
+    def match_query_input(self, ifile, qfile):
+        inputs = self.get_inputs(ifile)
         container = []
         for input_set in inputs:
-            q_group = self.get_query()
+            q_group = self.get_query(qfile)
             for query_set in q_group:
                 data = self.query_check(query_set, input_set)
-                if data != None:
+                if data is not None:
                     for d in data:
                         container.append((d, tuple(query_set)))
-        x = tuple(container)
-
-        container = Counter(x)
+        cnt = tuple(container)
+        container = Counter(cnt)
         m_c = container.most_common()
         results = [{item[0][0]:item[1]}for item in m_c]
 
         return self.output(results)
 
 
-    def query_check(self, qlist, ilist):
+    @staticmethod
+    def query_check(qlist, ilist):
         result = all([qs in ilist  for qs in qlist])
         if result:
             new_words = ilist - qlist
@@ -58,8 +60,7 @@ class Trainer():
     @staticmethod
     def output(dictionary):
         result = dumps(dictionary, sort_keys=True)
-        print result
         return result
 
-tr = Trainer()
-tr.match_query_input()
+#tr = Trainer()
+#print tr.match_query_input(tr.i_file, tr.q_file)
